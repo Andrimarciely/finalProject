668a4d8f83d6046e3b37fb452510191b5192adfb
Andrimarciely Souza <andrimarciely@gmail.com>
2022-12-07 19:41:12 -0400

    model atividade

--- data/models.py
+++ data/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Atividade(models.Model):
+    lista_prioridades = (
+        ('0','Sem prioridade'),
+        ('1', 'Normal'),
+        ('2', 'Urgente'),
+        ('3', 'Muito Urgente'),
+    )
+    lista_status = (
+        ('0', 'Pendente'),
+        ('1', 'Realizando'),
+        ('2', 'Concluído'),
+    )
+
+    date = models.DateField()
+    atividade = models.CharField(max_length=80)
+    prioridade = models.CharField(max_length=1,choices=lista_prioridades)
+    contratante = models.CharField(max_length=20)
+    responsavel = models.CharField(max_length=20)
+    status = models.CharField(max_length=1,choices=lista_status)
+
+    def __str__(self):
+        return self.atividade
